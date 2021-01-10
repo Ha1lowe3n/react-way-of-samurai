91098c9e9f2ad61ec661bb29fc857850b4ad94ab
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-01-10 23:56:44 +0800

    add props in Post

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,11 +11,10 @@ function MyPosts() {
       <div>
         <textarea></textarea>
         <button>Add post</button>
-        <button>Remove</button>
       </div>
       <div className={classes.posts}>
-        <Post />
-        <Post />
+        <Post message="Hello" />
+        <Post message="Hi" />
       </div>
     </div>
   );
