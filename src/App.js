fe9f1ca2b2df74ca89fa15b3c32b740d810e5dbb
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-01-10 18:54:38 +0800

    prototype site, add styles and layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import './app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://sun9-43.userapi.com/c624116/v624116060/250b7/WB6xXxDpBZA.jpg" />
       </header>
+
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+
+      <div className="content">
+        <div>
+          <img src="https://img3.goodfon.ru/original/1920x1200/a/f0/pesok-plyazh-sledy-more-palmy.jpg" alt="wrapper" />
+        </div>
+        <div>ava + descr</div>
+        <div>
+          my posts
+          <div>new post</div>
+          <div>1 post</div>
+          <div>2 post</div>
+        </div>
+
+      </div>
     </div>
   );
 }
