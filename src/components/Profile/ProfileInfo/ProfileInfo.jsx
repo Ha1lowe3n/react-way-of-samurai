9a3c3c72f94a06679c90b8fe667916123f619995
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-03-01 22:53:42 +0800

    fix bug

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -19,6 +19,10 @@ function ProfileInfo({ profile }) {
         <img src={profile.photos.large} />
         ava + descr
       </div>
+      <div className={classes.descr}>
+        <img src={profile.photos.large} />
+        ava + descr
+      </div>
     </div>
   );
 }
