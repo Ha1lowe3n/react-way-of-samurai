aabf4bbc8a3556f99354dc6bab82c5db46876177
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-03-03 21:36:02 +0800

    lesson 61: login network

---
+++ src/components/Header/HeaderContainer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import classes from "./Header.module.scss";
+
+function Header() {
+    return (
+        <header className={classes.header}>
+            <img
+                src="https://sun9-43.userapi.com/c624116/v624116060/250b7/WB6xXxDpBZA.jpg"
+                alt={"logo"}
+            />
+
+            <div className={classes.loginBlock}>
+                <NavLink to={"/login"}>Login</NavLink>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/redux/reducers/auth.js
@@ -0,0 +1,57 @@
+// action types
+const SEND_MESSAGE = 'SEND_MESSAGE';
+const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE_NEW_MESSAGE_TEXT';
+
+const initialState = {
+  dialogs: [
+    { id: 1, name: "Dima" },
+    { id: 2, name: "Oleg" },
+    { id: 3, name: "Sveta" },
+    { id: 4, name: "Valera" },
+  ],
+
+  messages: [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "Hello" },
+    { id: 3, message: "What's up?" },
+  ],
+
+  newMessageText: ""
+};
+
+const dialogs = (state = initialState, action) => {
+  switch (action.type) {
+    case SEND_MESSAGE:
+      const newMessage = {
+        id: 6,
+        message: state.newMessageText
+      }
+
+      return {
+        ...state,
+        messages: [...state.messages, newMessage],
+        newMessageText: ""
+      }
+
+    case UPDATE_NEW_MESSAGE_TEXT:
+      return {
+        ...state,
+        newMessageText: action.messageText
+      }
+
+    default:
+      return state;
+  }
+}
+
+// action creators
+export const sendMessageActionCreator = () => ({
+  type: SEND_MESSAGE,
+});
+
+export const updateNewMessageTextActionCreator = (text) => ({
+  type: UPDATE_NEW_MESSAGE_TEXT,
+  messageText: text
+});
+
+export default dialogs;
